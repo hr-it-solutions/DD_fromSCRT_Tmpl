b6096bc0c3833ec70268a7f21a95097b96651104
Florian Häusler <florian@didldu.com>
2017-07-20 19:12:02 +0200

    dd set cookie function implemented

--- js/template.js
+++ js/template.js
@@ -145,6 +145,14 @@
 
 };
 
+/* Set cookie (global) */
+function dd_setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 (function($,sr){
     // http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/
     // debouncing function from John Hann
